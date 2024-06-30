ee7245c10f88d4d26449cfe16488af2efaf8580f
rafaelomodei <omodei@alunos.utfpr.edu.br>
2024-06-30 15:58:43 -0300

    feat: added options multiple filters

--- helper.py
+++ helper.py
@@ -2,5 +2,7 @@ def parse_filters(filter_str):
     filters = {}
     for item in filter_str.split(','):
         filter_name, filter_value = item.split('=')
-        filters[filter_name.strip()] = float(filter_value.strip())
+        if filter_name.strip() not in filters:
+            filters[filter_name.strip()] = []
+        filters[filter_name.strip()].append(float(filter_value.strip()))
     return filters
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from helper import parse_filters
 def main():
     parser = argparse.ArgumentParser(description="Process images with multiple filters.")
     parser.add_argument('--dir', required=True, help="Source directory of images")
-    parser.add_argument('--filters', required=True, help="Filters to apply with values, e.g., 'blur=5.0, brightness=2.4'")
+    parser.add_argument('--filters', required=True, help="Filters to apply with values, e.g., 'blur=5.0,brightness=2.4'")
     args = parser.parse_args()
 
     source_directory = args.dir
@@ -20,16 +20,17 @@ def main():
         'saturation': SaturationFilter
     }
 
-    for filter_type, filter_value in filter_params.items():
+    for filter_type, values in filter_params.items():
         if filter_type in filter_classes:
-            image_filter = filter_classes[filter_type](filter_value)
+            for filter_value in values:
+                image_filter = filter_classes[filter_type](filter_value)
 
-            base_name = os.path.basename(source_directory.rstrip('/'))
-            parent_directory = os.path.dirname(source_directory.rstrip('/'))
-            destination_directory = os.path.join(parent_directory, f"{base_name}_{filter_type}_{filter_value}")
+                base_name = os.path.basename(source_directory.rstrip('/'))
+                parent_directory = os.path.dirname(source_directory.rstrip('/'))
+                destination_directory = os.path.join(parent_directory, f"{base_name}_{filter_type}_{filter_value}")
 
-            processor = ImageProcessor(image_filter, filter_type, filter_value)
-            processor.process_directory(source_directory, destination_directory)
+                processor = ImageProcessor(image_filter, filter_type, filter_value)
+                processor.process_directory(source_directory, destination_directory)
         else:
             print(f"Filter '{filter_type}' is not recognized.")
 
